8b46266fe1ab198172552a4177ab9977341af45d
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2025-04-02 22:12:26 -0300

    Creates base application and configure first liquibase migration

---
+++ src/main/java/com/justicehub/api/JusticeHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.justicehub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticeHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusticeHubApiApplication.class, args);
+	}
+
+}
